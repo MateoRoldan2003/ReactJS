b12128771c844ade40a78e9bf30a9f61f1412941
MateoRoldan2003 <134400903+MateoRoldan2003@users.noreply.github.com>
2023-10-05 19:14:11 -0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ function Navbar() {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/category/vehiculos">Vehículos</Link> {/* Cambiar 'vehiculos' por el ID real de la categoría */}
-      {/* ... Otros enlaces a categorías */}
+      <Link to="/category/vehiculos">Vehículos</Link> {}
+      {}
     </nav>
   );
 }
