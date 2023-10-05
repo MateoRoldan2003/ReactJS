30bd373543439f6aff0d1f9609e86b8200014439
MateoRoldan2003 <134400903+MateoRoldan2003@users.noreply.github.com>
2023-10-05 19:17:54 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa "Routes" en lugar de "Route"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './NavBnpm ar';
 import Catalog from './Catalog';
 import ProductDetail from './ProductDetail';
