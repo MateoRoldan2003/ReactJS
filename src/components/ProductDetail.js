41f2d5f77921980c4dd4801c1b545d00dc52f5e5
MateoRoldan2003 <134400903+MateoRoldan2003@users.noreply.github.com>
2023-10-05 19:15:14 -0300

    Update ProductDetail.js

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,24 +6,22 @@ function ProductDetail() {
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    // Aquí deberías hacer una solicitud a tu API o cargar detalles del producto según el itemId (id)
-    // Ejemplo de solicitud ficticia a una API:
     fetch(`/api/product/${id}`)
       .then((response) => response.json())
       .then((data) => setProduct(data))
-      .catch((error) => console.error('Error fetching data:', error));
+      .catch((error) => console.error('Error al obtener datos:', error));
   }, [id]);
 
   if (!product) {
-    return <div>Loading...</div>;
+    return <div>Cargando...</div>;
   }
 
   return (
     <div>
-      <h1>Product Detail</h1>
+      <h1>Producto</h1>
       <h2>{product.name}</h2>
-      <p>Description: {product.description}</p>
-      <p>Price: {product.price}</p>
+      <p>Descripcion: {product.description}</p>
+      <p>Precio: {product.price}</p>
     </div>
   );
 }
